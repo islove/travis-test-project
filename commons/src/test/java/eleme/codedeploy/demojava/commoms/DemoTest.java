f20f75509cd65f5b770fca5b071bdd0f637fefb4
吴云峰 <yunfeng.wu@ele.me>
2015-11-12 18:19:56 +0800

    test

--- commons/src/test/java/eleme/codedeploy/demojava/commoms/DemoTest.java
+++ commons/src/test/java/eleme/codedeploy/demojava/commoms/DemoTest.java
@@ -4,10 +4,24 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import eleme.codedeploy.demojava.commons.Files;
+import eleme.codedeploy.demojava.commons.ValidateService;
+
 public class DemoTest {
+	
 	@Test  
     public void test1(){  
-        System.out.println("junit is executed.");  
-        assertTrue("Result 为true ", true); 
+        System.out.println("junit test1 is executed."); 
+        Files files = new Files();
+        files.setDestination("dog.liao");
+        assertTrue("Result 为true ", "dog.liao".equals(files.getDestination())); 
+    }  
+	
+	@Test  
+    public void test2(){  
+        System.out.println("junit test2 is executed."); 
+        ValidateService validateService = new ValidateService();
+        validateService.setLocation("dog.liao");
+        assertTrue("Result 为true ", "dog.liao".equals(validateService.getLocation())); 
     }  
 }
