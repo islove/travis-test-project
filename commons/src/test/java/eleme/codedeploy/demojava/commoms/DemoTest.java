5dd718d4e0cbbf3b6085479e684d6709a41869e1
吴云峰 <yunfeng.wu@ele.me>
2015-11-12 18:05:21 +0800

    test

---
+++ commons/src/test/java/eleme/codedeploy/demojava/commoms/DemoTest.java
@@ -0,0 +1,13 @@
+package eleme.codedeploy.demojava.commoms;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DemoTest {
+	@Test  
+    public void test1(){  
+        System.out.println("junit is executed.");  
+        assertTrue("Result 为true ", true); 
+    }  
+}
